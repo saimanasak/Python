0002352edbcdcec638cb33e49c87343a5ff6c9c8
Sai Manasa <muppedanvesh@gmail.com>
2023-05-18 02:01:45 -0500

    Final Update

---
+++ udemycourse/100_days/day1_band_name_generator.py
@@ -0,0 +1,15 @@
+Pink = '\033[95m'
+End = '\033[m'
+Cyan = '\033[36m'
+Green = '\033[32m'
+
+def band_name_generator():
+    
+    print(Pink + "\nWelcome to the Band Name Generator...\n", End)
+    
+    city = input("Enter your city: ")
+    pet = input("Enter your pet name: ")
+
+    print(Cyan + "\nYour band name could be:", End + Green + f"{city + ' ' + pet}", End)
+    
+band_name_generator()
