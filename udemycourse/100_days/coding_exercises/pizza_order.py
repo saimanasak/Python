f03c416fc64ab8d283eab86005635772b519755c
Sai Manasa <muppedanvesh@gmail.com>
2023-08-13 18:25:33 -0500

    Final Update

--- udemycourse/100_days/coding_exercises/pizza_order.py
+++ udemycourse/100_days/coding_exercises/pizza_order.py
@@ -1,3 +1,17 @@
+'''
+Congratulations, you've got a job at Python Pizza. Your first job is to build an automatic pizza order program.
+
+Based on a user's order, work out their final bill.
+
+Small Pizza: $15
+Medium Pizza: $20
+Large Pizza: $25
+Pepperoni for Small Pizza: +$2
+Pepperoni for Medium or Large Pizza: +$3
+Extra cheese for any size pizza: + $1
+
+'''
+
 Pink = '\033[95m'
 End = '\033[m'
 
