a29950f126a5debc0fe99515bb5efbb5580e81e5
Sai Manasa <muppedanvesh@gmail.com>
2023-08-13 21:50:43 -0500

    Final Update

--- udemycourse/100_days/coding_exercises/head_tails.py
+++ udemycourse/100_days/coding_exercises/head_tails.py
@@ -5,11 +5,13 @@ Write a virtual coin toss program. It will randomly tell the user "Heads" or "Ta
 Important, the first letter should be capitalised and spelt exactly like in the example e.g. Heads, not heads.
 
 '''
-
+#Imported random module:
 import random
 
+#Generates a random number and stores in a variable:
 toss = random.randint(0, 1)
 
+#Checks the conditions and prints either Heads or Tails:
 if toss == 0:
     print("\n{} - Tails\n".format(toss))
 else:
